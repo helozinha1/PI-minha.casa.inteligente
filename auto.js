f34c16a778d56b8e26ce8f30db5e3681265112f5
Helloysa Rocha <helowzx@gmail.com>
2025-06-06 11:28:05 -0300

    Versão 0.0.1 - Base

---
+++ auto.js
@@ -0,0 +1,9 @@
+let sensor_quarto = true
+let luz_ambiente = 20
+
+if (luz_ambiente < 20 || sensor_quarto == true){
+  console.log("Luz Liga")
+}
+else {
+    console.log ("Luz permanece desligada")
+}
